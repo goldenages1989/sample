3c891fe611ddd37e656598f36f8bc9baa2d33eed
glennzhou <xiaogang19891001@gmail.com>
2018-06-18 23:31:17 +0000

    finish edit update index delete actions

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Http\Request;
 use Auth;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth',[
+            'except' => ['show','create','store','index']
+        ]);
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index',compact('users'));
+    }
+
+
     //
     public function create()
     {
@@ -17,6 +35,39 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    public function update(User $user, Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        try {
+            $this->authorize('update', $user);
+
+            $data = [
+                'name' => $request->name
+            ];
+
+            if($request->password) {
+                $data['password'] = bcrypt($request->password);
+            }
+
+            $user->update($data);
+
+            session()->flash('success','更新成功');
+
+            return redirect()->route('users.show', $user->id);
+        } catch(AuthenticationException $e) {
+            return abort(403, '您无权访问');
+        }
     }
 
     public function store(Request $request)
@@ -36,6 +87,16 @@ class UsersController extends Controller
         Auth::login($user);
         session()->flash('success','A new jour has begin!');
         return redirect()->route('users.show',[$user]);
+    }
+
+    public function destroy(User $user)
+    {
+//        $this->authorize('destroy', $user);
+
+        $user->delete();
+
+        session()->flash('success','删除用户成功');
+        return redirect()->back();
     }
 
 }
--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            session()->flash('info','Should not login again');
+            return redirect('/');
         }
 
         return $next($request);
